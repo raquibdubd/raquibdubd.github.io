ea5ab8ac4dc9d33bb946f621a2b7bfb22f1fb1bd
raquibh <raquib_dubd@yahoo.com>
2025-03-26 20:03:00 +0600

    231

--- quotes/mathquotes.js
+++ quotes/mathquotes.js
@@ -1,5 +1,9 @@
 function myFunction() {
 var items = Array(523, 3452, 334, 31, 5346);
 var item = items[Math.floor(Math.random()*items.length)];
+document.getElementById("demo1").innerHTML =item;   
 return item;
 }
+
+// Varible call
+var name = "Raquib";
